5743be807f2504893b9cbae7e5fa4f06eb5af596
Codexzier <johannes.paul.langner@live.de>
2021-10-11 18:47:08 +0200

    Test Bezüglich des SideHostControl

---
+++ Codexzier.Wpf.ApplicationFramework.Test/Components/Ui/Helpers/CustomHelperTest.cs
@@ -0,0 +1,29 @@
+﻿using Codexzier.Wpf.ApplicationFramework.Components.Ui.EventBus;
+using Codexzier.Wpf.ApplicationFramework.Components.Ui.Helpers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Codexzier.Wpf.ApplicationFramework.Test.Components.Ui.Helpers
+{
+    [TestFixture, SingleThreaded]
+    public class CustomHelperTest
+    {
+        [Test, STAThread]
+        public void GetContentReference()
+        {
+            // TODO: Probelm mit dem STA. Eigentlich hatte ich dazu schonmal eine Lösung.
+
+            //// arrange
+            //var customControl = new SideHostControl();
+
+            //// act
+            //var result = customControl.GetContentPresenter<ContentPresenter>();
+
+            //// assert
+            //Assert.IsNotNull(result);
+        }
+    }
+}
