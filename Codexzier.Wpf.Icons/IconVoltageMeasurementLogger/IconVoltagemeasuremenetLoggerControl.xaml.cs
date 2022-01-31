1d255156396c9e4c3edf849d24217aabe8e65bf6
Codexzier <johannes.paul.langner@live.de>
2022-01-31 18:49:49 +0100

    Zwischenstand

---
+++ Codexzier.Wpf.Icons/IconVoltageMeasurementLogger/IconVoltagemeasuremenetLoggerControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Codexzier.Wpf.Icons.IconVoltageMeasurementLogger
+{
+    /// <summary>
+    /// Interaction logic for IconVoltagemeasuremenetLoggerControl.xaml
+    /// </summary>
+    public partial class IconVoltagemeasuremenetLoggerControl : UserControl
+    {
+        public IconVoltagemeasuremenetLoggerControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
