a46cd62e88d54a3b22808faab81e3e0a6c4799dc
Codexzier <johannes.paul.langner@live.de>
2022-02-16 19:24:28 +0100

    Add new Color Schemas to change color look

---
+++ Codexzier.Wpf.ApplicationFramework/Styles/ColorMonochromaticSetup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Codexzier.Wpf.ApplicationFramework.Styles
+{
+    public class ColorMonochromaticSetup
+    {
+        private static ResourceDictionary _resourceDictionary = new ResourceDictionary
+        {
+            Source = new Uri("/Codexzier.Wpf.ApplicationFramework;component/Styles/Colors.xaml")
+        };
+
+        public static Monochromatic ColorScheme { get; set; }
+
+
+        public Color ColorMonochromaticA()
+        {
+            switch (ColorScheme)
+            {
+                case Monochromatic.BlueGray:
+                    {
+                        var c = _resourceDictionary["ColorMonochromaticBlueGrayA"] as Color?;
+
+                        if(c is null)
+                        {
+                            c = Color.FromRgb(100, 0, 0);
+                        }
+
+                        return (Color) c; //ColorMonochromaticBlueGrayA;
+}
+            }
+
+            return Color.FromRgb(100, 0, 0);
+        }
+
+        public static Color ColorMonochromaticB { get; set; }
+        public static Color ColorMonochromaticC { get; set; }
+        public static Color ColorMonochromaticD { get; set; }
+        public static Color ColorMonochromaticE { get; set; }
+    }
+
+    public enum Monochromatic
+    {
+        BlueGray
+    }
+}
